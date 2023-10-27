c997cacbf2dcff89f960b74f30ca93045a09ef28
theederrickmaina <146960594+theederrickmaina@users.noreply.github.com>
2023-10-27 15:02:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); 
+}
+
+showSlides(); 
